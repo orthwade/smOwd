11cd4a89a152e45068444c90e1f01066395bfab0
owd <orthwade@protonmail.com>
2025-01-22 01:15:53 +0500

    working

--- user/user.go
+++ user/user.go
@@ -16,6 +16,7 @@ type User struct {
 	UserName     string
 	LanguageCode string
 	IsBot        bool
+	Enabled      bool
 }
 
 func StoreUser(ctx context.Context, db *sql.DB, u User) error {
@@ -24,12 +25,14 @@ func StoreUser(ctx context.Context, db *sql.DB, u User) error {
 		logger = logs.New(slog.New(slog.NewTextHandler(os.Stderr, nil)))
 	}
 	query := `
-		INSERT INTO users (telegram_id, first_name, last_name, user_name, language_code, is_bot)
-		VALUES ($1, $2, $3, $4, $5, $6)
+		INSERT INTO users (telegram_id, first_name, last_name, user_name, language_code, is_bot, enabled)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)
 		ON CONFLICT (telegram_id) DO NOTHING;
 	`
 
-	_, err := db.Exec(query, u.TelegramID, u.FirstName, u.LastName, u.UserName, u.LanguageCode, u.IsBot)
+	_, err := db.Exec(query, u.TelegramID, u.FirstName, u.LastName, u.UserName,
+		u.LanguageCode, u.IsBot, u.Enabled)
+
 	if err != nil {
 		logger.Error("Failed to store user", "error", err)
 		return err
@@ -59,6 +62,7 @@ func GetUserByTelegramID(ctx context.Context, db *sql.DB, telegramID int) (*User
 		&u.UserName,
 		&u.LanguageCode,
 		&u.IsBot,
+		&u.Enabled,
 	)
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -71,4 +75,27 @@ func GetUserByTelegramID(ctx context.Context, db *sql.DB, telegramID int) (*User
 
 	logger.Info(fmt.Sprintf("User with TelegramID %d retrieved successfully", telegramID))
 	return &u, nil
+}
+
+func RemoveUser(ctx context.Context, db *sql.DB, telegramID int) error {
+	logger, ok := ctx.Value("logger").(*logs.Logger)
+
+	if !ok {
+		logger = logs.New(slog.New(slog.NewTextHandler(os.Stderr, nil)))
+	}
+
+	query := `
+		DELETE FROM users
+		WHERE telegram_id = $1;
+	`
+
+	_, err := db.ExecContext(ctx, query, telegramID)
+
+	if err != nil {
+		logger.Error("Failed to delete user", "error", err, "Telegram ID", telegramID)
+	} else {
+		logger.Info("Deleted user", "Telegram ID", telegramID)
+	}
+
+	return err
 }
