80345891a019b8dc858dc834da91445e1aba4d32
owd <orthwade@protonmail.com>
2025-02-08 21:09:15 +0500

    working on docker

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Check if the database already exists
+echo "DB_SUPERUSER=${DB_SUPERUSER}"
+echo "DB_NAME=${DB_NAME}"
+echo "DB_USER=${DB_USER}"
+
+# Check if the database exists
 echo "Checking if database exists"
 psql -U ${DB_SUPERUSER} -d postgres -tAc "SELECT 1 FROM pg_database WHERE datname='${DB_NAME}'" | grep -q 1 || {
   # If database does not exist, create it
@@ -8,16 +12,10 @@ psql -U ${DB_SUPERUSER} -d postgres -tAc "SELECT 1 FROM pg_database WHERE datnam
   psql -U ${DB_SUPERUSER} -d postgres -c "CREATE DATABASE ${DB_NAME}"
 }
 
-# Create the user if it doesn't exist
+# Check if the user exists
 echo "Checking if user exists"
 psql -U ${DB_SUPERUSER} -d postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='${DB_USER}'" | grep -q 1 || {
   echo "Creating user ${DB_USER}"
   psql -U ${DB_SUPERUSER} -d postgres -c "CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASSWORD}'"
   psql -U ${DB_SUPERUSER} -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER}"
 }
-
-# Optionally, you can run additional SQL queries if required
-# You can put your SQL file in here or run any needed query
-# Example (uncomment the following lines to run init.sql.template or other SQL commands):
-
-# psql -U ${DB_SUPERUSER} -d ${DB_NAME} -f /docker-entrypoint-initdb.d/init.sql.template
